74091fb83c9f68a5d12fe6adb37ba15bca254d8f
jlbettha <5200190+jlbettha@users.noreply.github.com>
2025-06-25 19:00:52 -0400

    uv clear

--- data_compression/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from data-compression!")
-
-
-if __name__ == "__main__":
-    main()
--- my_modules/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from my-modules!")
-
-
-if __name__ == "__main__":
-    main()
