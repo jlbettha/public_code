8d9e78091488ff199ae53686af07915ed6225d88
jlbettha <5200190+jlbettha@users.noreply.github.com>
2025-07-19 08:25:17 -0400

    Update my_losses.py

--- torch_models/my_losses.py
+++ torch_models/my_losses.py
@@ -1,28 +1,34 @@
+from typing import Optional, List
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.loss import _Loss
+from ._functional import soft_dice_score, to_tensor
+from .constants import BINARY_MODE, MULTICLASS_MODE, MULTILABEL_MODE
 
-class DiceLoss(nn.Module):
-    def forward(self, inputs, targets, smooth=1):
-        inputs = torch.sigmoid(inputs)
-        intersection = (inputs * targets).sum()
-        dice = (2.*intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
-        return 1 - dice
+# class DiceLoss(nn.Module):
+#     def forward(self, inputs, targets, smooth=1):
+#         inputs = torch.sigmoid(inputs)
+#         intersection = (inputs * targets).sum()
+#         dice = (2.*intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
+#         return 1 - dice
 
-# TO-DO: 
+# TO-DO:
 # class FocalLoss(nn.Module):
 #     def forward(self, inputs, targets, smooth=1):
 #         inputs = torch.sigmoid(inputs)
 #         intersection = (inputs * targets).sum()
 #         dice = (2.*intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
 #         return 1 - dice
-    
-    
+
+
 class TverskyLoss(nn.Module):
     def __init__(self, alpha=0.5, beta=0.5):
         super().__init__()
         self.alpha = alpha
         self.beta = beta
-    
+
     def forward(self, inputs, targets, smooth=1):
         inputs = torch.sigmoid(inputs)
         tp = (inputs * targets).sum()
@@ -30,15 +36,15 @@ class TverskyLoss(nn.Module):
         fn = (targets * (1 - inputs)).sum()
         tversky = (tp + smooth) / (tp + self.alpha * fp + self.beta * fn + smooth)
         return 1 - tversky
-    
-    
+
+
 # TO-DO:
 # class FocalTverskyLoss(nn.Module):
 #     def __init__(self, alpha=0.5, beta=0.5, gamma=2):
 #         super().__init__()
 #         self.alpha = alpha
 #         self.beta = beta
-#         self.gamma = gamma    
+#         self.gamma = gamma
 #     def forward(self, inputs, targets, smooth=1):
 #         inputs = torch.sigmoid(inputs)
 #         intersection = (inputs * targets).sum()
@@ -53,3 +59,166 @@ class TverskyLoss(nn.Module):
 #         intersection = (inputs * targets).sum()
 #         dice = (2.*intersection + smooth) / (inputs.sum() + targets.sum() + smooth)
 #         return 1 - dice
+
+
+class DiceLoss(_Loss):
+    def __init__(
+        self,
+        mode: str,
+        classes: Optional[List[int]] = None,
+        log_loss: bool = False,
+        from_logits: bool = True,
+        smooth: float = 0.0,
+        ignore_index: Optional[int] = None,
+        eps: float = 1e-7,
+    ):
+        """Dice loss for image segmentation task.
+        It supports binary, multiclass and multilabel cases
+
+        Args:
+            mode: Loss mode 'binary', 'multiclass' or 'multilabel'
+            classes:  List of classes that contribute in loss computation. By default, all channels are included.
+            log_loss: If True, loss computed as `- log(dice_coeff)`, otherwise `1 - dice_coeff`
+            from_logits: If True, assumes input is raw logits
+            smooth: Smoothness constant for dice coefficient (a)
+            ignore_index: Label that indicates ignored pixels (does not contribute to loss)
+            eps: A small epsilon for numerical stability to avoid zero division error
+                (denominator will be always greater or equal to eps)
+
+        Shape
+             - **y_pred** - torch.Tensor of shape (N, C, H, W)
+             - **y_true** - torch.Tensor of shape (N, H, W) or (N, C, H, W)
+
+        Reference
+            https://github.com/BloodAxe/pytorch-toolbelt
+        """
+        assert mode in {BINARY_MODE, MULTILABEL_MODE, MULTICLASS_MODE}
+        super(DiceLoss, self).__init__()
+        self.mode = mode
+        if classes is not None:
+            assert (
+                mode != BINARY_MODE
+            ), "Masking classes is not supported with mode=binary"
+            classes = to_tensor(classes, dtype=torch.long)
+
+        self.classes = classes
+        self.from_logits = from_logits
+        self.smooth = smooth
+        self.eps = eps
+        self.log_loss = log_loss
+        self.ignore_index = ignore_index
+
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
+        assert y_true.size(0) == y_pred.size(0)
+
+        if self.from_logits:
+            # Apply activations to get [0..1] class probabilities
+            # Using Log-Exp as this gives more numerically stable result and does not cause vanishing gradient on
+            # extreme values 0 and 1
+            if self.mode == MULTICLASS_MODE:
+                y_pred = y_pred.log_softmax(dim=1).exp()
+            else:
+                y_pred = F.logsigmoid(y_pred).exp()
+
+        bs = y_true.size(0)
+        num_classes = y_pred.size(1)
+        dims = (0, 2)
+
+        if self.mode == BINARY_MODE:
+            y_true = y_true.reshape(bs, 1, -1)
+            y_pred = y_pred.reshape(bs, 1, -1)
+
+            if self.ignore_index is not None:
+                mask = y_true != self.ignore_index
+                y_pred = y_pred * mask
+                y_true = y_true * mask
+
+        if self.mode == MULTICLASS_MODE:
+            y_true = y_true.reshape(bs, -1)
+            y_pred = y_pred.reshape(bs, num_classes, -1)
+
+            if self.ignore_index is not None:
+                mask = y_true != self.ignore_index
+                y_pred = y_pred * mask.unsqueeze(1)
+
+                y_true = F.one_hot(
+                    (y_true * mask).to(torch.long), num_classes
+                )  # N,H*W -> N,H*W, C
+                y_true = y_true.permute(0, 2, 1) * mask.unsqueeze(1)  # N, C, H*W
+            else:
+                y_true = F.one_hot(y_true, num_classes)  # N,H*W -> N,H*W, C
+                y_true = y_true.permute(0, 2, 1)  # N, C, H*W
+
+        if self.mode == MULTILABEL_MODE:
+            y_true = y_true.reshape(bs, num_classes, -1)
+            y_pred = y_pred.reshape(bs, num_classes, -1)
+
+            if self.ignore_index is not None:
+                mask = y_true != self.ignore_index
+                y_pred = y_pred * mask
+                y_true = y_true * mask
+
+        scores = self.compute_score(
+            y_pred, y_true.type_as(y_pred), smooth=self.smooth, eps=self.eps, dims=dims
+        )
+
+        if self.log_loss:
+            loss = -torch.log(scores.clamp_min(self.eps))
+        else:
+            loss = 1.0 - scores
+
+        # Dice loss is undefined for empty images with no classes
+        # So we set the contribution of any channel without true pixels to zero
+        # NOTE: A better workaround would be to use loss term `mean(y_pred)`
+        # for this case, however it will be a modified jaccard loss
+
+        mask = y_true.sum(dims) > 0
+        loss *= mask.to(loss.dtype)
+
+        if self.classes is not None:
+            loss = loss[self.classes]
+
+        return self.aggregate_loss(loss)
+
+    def aggregate_loss(self, loss):
+        return loss.mean()
+
+    def compute_score(
+        self, output, target, smooth=0.0, eps=1e-7, dims=None
+    ) -> torch.Tensor:
+        return soft_dice_score(output, target, smooth, eps, dims)
+
+
+class MulticlassDiceLoss(nn.Module):
+    """Reference: https://www.kaggle.com/code/bigironsphere/loss-function-library-keras-pytorch#Dice-Loss"""
+
+    def __init__(self, num_classes, softmax_dim=None):
+        super().__init__()
+        self.num_classes = num_classes
+        self.softmax_dim = softmax_dim
+
+    def forward(self, logits, targets, reduction="mean", smooth=1e-6):
+        """The "reduction" argument is ignored. This method computes the dice
+        loss for all classes and provides an overall weighted loss.
+        """
+        probabilities = logits
+        if self.softmax_dim is not None:
+            probabilities = nn.Softmax(dim=self.softmax_dim)(logits)
+        # end if
+        targets_one_hot = torch.nn.functional.one_hot(
+            targets, num_classes=self.num_classes
+        )
+        print(targets_one_hot.shape)
+        # Convert from NHWC to NCHW
+        targets_one_hot = targets_one_hot.permute(0, 3, 1, 2)
+
+        # Multiply one-hot encoded ground truth labels with the probabilities to get the
+        # prredicted probability for the actual class.
+        intersection = (targets_one_hot * probabilities).sum()
+
+        mod_a = intersection.sum()
+        mod_b = targets.numel()
+
+        dice_coefficient = 2.0 * intersection / (mod_a + mod_b + smooth)
+        dice_loss = -dice_coefficient.log()
+        return dice_loss
